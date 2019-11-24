3d4641a8ba7f327b652e35dd1089d65f76388a5f
Fey Naomi Schrewe <fey@posteo.eu>
2019-11-24 20:21:36 +0100

    Fix pronoun sql files

--- sql/pronouns4.get.sql
+++ sql/pronouns4.get.sql
@@ -2,7 +2,7 @@ SELECT pronouns.cases, languages.name AS language, languages.iso_639_3
 FROM pronouns
 INNER JOIN languages ON languages.iso_639_3 = pronouns.language
 WHERE (LOWER(languages.name) = LOWER($1) OR LOWER(languages.iso_639_1) = LOWER($1) OR LOWER(languages.iso_639_3) = LOWER($1))
-  AND LOWER($2) = ANY(pronouns.cases);
-  AND LOWER($3) = ANY(pronouns.cases);
-  AND LOWER($4) = ANY(pronouns.cases);
+  AND LOWER($2) = ANY(pronouns.cases)
+  AND LOWER($3) = ANY(pronouns.cases)
+  AND LOWER($4) = ANY(pronouns.cases)
   AND LOWER($5) = ANY(pronouns.cases);
