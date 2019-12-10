6f35136c83ab9795ceb51864c5c76db63e49f208
Fey Naomi Schrewe <fey@posteo.eu>
2019-12-10 19:30:10 +0100

    Add Swedish pronouns

---
+++ migrations/007.do.swedish.sql
@@ -0,0 +1,4 @@
+INSERT INTO pronouns (language, cases) VALUES ('swe', '{hen, hen, hens}');
+INSERT INTO pronouns (language, cases) VALUES ('swe', '{hen, henom, hens}');
+INSERT INTO pronouns (language, cases) VALUES ('swe', '{han, henne, henne}');
+INSERT INTO pronouns (language, cases) VALUES ('swe', '{hon, honom, hans}');
---
+++ migrations/007.undo.swedish.sql
@@ -0,0 +1,4 @@
+DELETE FROM pronouns WHERE language = 'swe' AND cases = '{hen, hen, hens}';
+DELETE FROM pronouns WHERE language = 'swe' AND cases = '{hen, henom, hens}';
+DELETE FROM pronouns WHERE language = 'swe' AND cases = '{han, henne, henne}';
+DELETE FROM pronouns WHERE language = 'swe' AND cases = '{hon, honom, hans}';
