a8d2345435b75e3a1d4bf4313f6ab866b41c7d19
Fey Naomi Schrewe <fey@posteo.eu>
2020-06-25 18:29:22 +0200

    Add Esperanto pronouns

---
+++ migrations/011.do.esperanto.sql
@@ -0,0 +1,5 @@
+INSERT INTO pronouns (language, cases) VALUES ('epo', '{ri}');
+INSERT INTO pronouns (language, cases) VALUES ('epo', '{ŝi}');
+INSERT INTO pronouns (language, cases) VALUES ('epo', '{ĝi}');
+INSERT INTO pronouns (language, cases) VALUES ('epo', '{li}');
+INSERT INTO pronouns (language, cases) VALUES ('epo', '{hi}');
