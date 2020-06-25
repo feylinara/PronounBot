31ecac116162b02f06bdb94dc4b7c13f2f53b6e8
Fey Naomi Schrewe <fey@posteo.eu>
2020-06-25 17:39:59 +0200

    Add no pronoun options for some languages

--- migrations/009.do.no-pronouns.sql
+++ migrations/009.do.no-pronouns.sql
@@ -1,3 +1,6 @@
+-- a hack so I don't have to do internationalisation yet.
+ALTER TABLE pronouns ALTER COLUMN cases TYPE character varying (32) ARRAY;
+
 INSERT INTO pronouns (language, cases) VALUES ('deu', '{Keine Pronomen}');
 INSERT INTO pronouns (language, cases) VALUES ('eng', '{no pronouns}');
 INSERT INTO pronouns (language, cases) VALUES ('nld', '{geen voornaamworden}');
--- migrations/009.undo.no-pronouns.sql
+++ migrations/009.undo.no-pronouns.sql
@@ -1,4 +1,4 @@
 DELETE FROM pronouns WHERE language = 'deu' and cases = '{Keine Pronomen}';
 DELETE FROM pronouns WHERE language = 'eng' and cases = '{no pronouns}';
-DELETE FROM pronouns WHERE language = 'nld' and cases = '{geen voornaamworden}';
+DELETE FROM pronouns WHERE language = 'nld' and cases = '{geen pronomina}';
 DELETE FROM pronouns WHERE language = 'fra' and cases = '{pas des pronoms}';
