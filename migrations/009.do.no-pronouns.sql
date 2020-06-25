fbbb53bafd6e9d5e7529bf335e3a2d1265d7e9ca
Fey Naomi Schrewe <fey@posteo.eu>
2020-06-25 17:28:30 +0200

    Add no pronoun options for some languages

---
+++ migrations/009.do.no-pronouns.sql
@@ -0,0 +1,4 @@
+INSERT INTO pronouns (language, cases) VALUES ('deu', '{Keine Pronomen}');
+INSERT INTO pronouns (language, cases) VALUES ('eng', '{no pronouns}');
+INSERT INTO pronouns (language, cases) VALUES ('nld', '{geen voornaamworden}');
+INSERT INTO pronouns (language, cases) VALUES ('fra', '{pas des pronoms}');
