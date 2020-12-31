2bd44817aacf5ee688c296c3722a7c26574601cd
Fey Naomi Schrewe <fey@posteo.eu>
2020-12-31 22:57:27 +0100

    add mandarin pronouns

---
+++ migrations/013.do.chinese.sql
@@ -0,0 +1,7 @@
+INSERT INTO pronouns (language, cases) VALUES ('zho', '{ta}');
+INSERT INTO pronouns (language, cases) VALUES ('zho', '{他}');
+INSERT INTO pronouns (language, cases) VALUES ('zho', '{它}');
+INSERT INTO pronouns (language, cases) VALUES ('zho', '{祂}');
+INSERT INTO pronouns (language, cases) VALUES ('zho', '{牠}');
+INSERT INTO pronouns (language, cases) VALUES ('zho', '{她}');
+INSERT INTO pronouns (language, cases) VALUES ('zho', '{ta}');
