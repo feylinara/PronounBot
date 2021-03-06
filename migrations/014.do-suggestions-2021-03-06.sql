8b9a41498aa9cd58ee7e549f5acc1985df7e2a3e
Fey Naomi Schrewe <fey@posteo.eu>
2021-03-06 11:28:49 +0100

    Add some more pronouns

---
+++ migrations/014.do-suggestions-2021-03-06.sql
@@ -0,0 +1,4 @@
+INSERT INTO pronouns (language, cases) VALUES ('deu', '{dey, dessen}');
+INSERT INTO pronouns (language, cases) VALUES ('deu', '{Name als Pronomen}');
+INSERT INTO pronouns (language, cases) VALUES ('eng', '{name as pronoun}');
+INSERT INTO pronouns (language, cases) VALUES ('nld', '{name ipv voornamwoord}');
