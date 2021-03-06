9a6493ee0d549ba5179643200e9927c2c7c6611e
Fey Naomi Schrewe <fey@posteo.eu>
2021-03-06 11:48:14 +0100

    Testing

--- src/index.js
+++ src/index.js
@@ -63,10 +63,8 @@ discordClient.on('message', async (message) => {
 
     // split by space keeping quote-wrapped strings
     let parse = message.content.match(/(?:[^\s"]+|"[^"]*")+/g);
-	      console.log(parse);
     if (parse) {
       parse = parse.map((x) => x.replace(/"/g, '').toLowerCase());
-	      console.log(parse);
       if (parse[0] == `${serverSettings.prefix}${commandWord}`) {
         if (parse[1] == 'add') {
           if (parse.length < 3) {
